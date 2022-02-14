1f8cadb3ab5e0b28c009482eabc825022b693e84
Soumya Nayar <nayar.s@northeastern.edu>
2022-02-13 23:08:27 -0800

    Adding validation to get, put and post methods

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -18,7 +18,9 @@ const comparePassword = async (password, hashedPassword) => {
 const authorizeToken = async (req, res, next) => {
   let authHeader = req.headers.authorization;
   if (!authHeader) {
-    return res.status(401).send("Unauthorized");
+    return res.status(401).json({
+      message: "Missing authorization header",
+    });
   }
   let { username, password } = getUserPasswordAuth(authHeader);
   let user = await User.findOne({
@@ -27,11 +29,15 @@ const authorizeToken = async (req, res, next) => {
     },
   });
   if (!user) {
-    return res.status(401).send("Unauthorized");
+    return res.status(401).json({
+      message: "Unauthorized: Invalid username or password",
+    });
   }
   let isPasswordMatch = await comparePassword(password, user.password);
   if (!isPasswordMatch) {
-    return res.status(401).send("Unauthorized");
+    return res.status(401).json({
+      message: "Unauthorized: Invalid username or password",
+    });
   }
   req.user = user;
   next();
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -24,8 +24,26 @@ const formatUser = (user) => {
 
 // POST /v1/user : create a new user and store in the db
 userRouter.post("/", async (req, res) => {
+  // validation for the user object id, account_created, account_updated fields should not be sent in the request body
+  if (req.body.id || req.body.account_created || req.body.account_updated) {
+    return res.status(400).json({
+      message: "id, account_created, and account_updated cannot be set",
+    });
+  }
+  // validation for the user object, username, password, first_name, last_name can't be empty
+  if (
+    !req.body.username ||
+    !req.body.password ||
+    !req.body.first_name ||
+    !req.body.last_name
+  ) {
+    return res.status(400).json({
+      message: "username, password, first_name, and last_name are required",
+    });
+  }
+
   const hashedPassword = await hashifyPassword(req.body.password);
-  //create a new user
+  // create a new user
   const user = User.build({
     id: uuid.v4(),
     username: req.body.username,
@@ -37,12 +55,22 @@ userRouter.post("/", async (req, res) => {
   });
 
   try {
-    //store the user in the db and return the user
+    // store the user in the db and return the user
     let result = await user.save();
     let formattedUser = formatUser(result.dataValues);
     return res.status(201).json(formattedUser);
   } catch (err) {
-    res.status(500).send(err);
+    // If username already exists, return a 400 error
+    if (err.name === "SequelizeUniqueConstraintError") {
+      return res.status(400).json({ message: "Username already exists" });
+    } else if (err.name === "SequelizeValidationError") {
+      return res.status(400).json({
+        message:
+          "Invalid username, Username should be an email Ex: abc@domain.com",
+      });
+    } else {
+      return res.status(500).json({ message: "Internal server error" });
+    }
   }
 });
 
@@ -51,12 +79,29 @@ userRouter.get("/self", authorizeToken, async (req, res) => {
     let formattedUser = formatUser(req.user.dataValues);
     return res.status(200).json(formattedUser);
   } catch (err) {
-    res.status(500).send(err);
+    return res.status(500).json({ message: "Internal server error" });
   }
 });
 
 userRouter.put("/self", authorizeToken, async (req, res) => {
   let user = req.user;
+  if (
+    req.body.id ||
+    req.body.username ||
+    req.body.account_updated ||
+    req.body.account_created
+  ) {
+    return res.status(400).json({
+      message:
+        "id, username, account_created, and account_updated cannot be set",
+    });
+  }
+
+  if (!(req.body.password || req.body.first_name || req.body.last_name)) {
+    return res.status(400).json({
+      message: "password, first_name, or last_name are required",
+    });
+  }
 
   user.set({
     first_name: req.body.first_name || user.first_name,
@@ -71,7 +116,7 @@ userRouter.put("/self", authorizeToken, async (req, res) => {
     await user.save();
     res.status(204).send();
   } catch (err) {
-    res.status(500).send(err);
+    return res.status(500).json({ message: "Internal server error" });
   }
 });
 
