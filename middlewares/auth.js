8b2a6b8822390433b8ae3357ac463406d9fa375d
Soumya Nayar <nayar.s@northeastern.edu>
2022-02-15 12:25:26 -0800

    Dependency Injection Bug fixes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,33 +1,36 @@
-const bcrypt = require("bcrypt");
-const db = require("../configs/db");
-const User = db.User;
+module.exports = (User) => {
+  const {
+    getUserPasswordAuth,
+    comparePassword,
+  } = require("../utils/authHelpers");
 
-const authorizeToken = async (req, res, next) => {
-  let authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).json({
-      message: "Missing authorization header",
+  const authorizeToken = async (req, res, next) => {
+    let authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).json({
+        message: "Missing authorization header",
+      });
+    }
+    let { username, password } = getUserPasswordAuth(authHeader);
+    let user = await User.findOne({
+      where: {
+        username: username,
+      },
     });
-  }
-  let { username, password } = getUserPasswordAuth(authHeader);
-  let user = await User.findOne({
-    where: {
-      username: username,
-    },
-  });
-  if (!user) {
-    return res.status(401).json({
-      message: "Unauthorized: Invalid username or password",
-    });
-  }
-  let isPasswordMatch = await comparePassword(password, user.password);
-  if (!isPasswordMatch) {
-    return res.status(401).json({
-      message: "Unauthorized: Invalid username or password",
-    });
-  }
-  req.user = user;
-  next();
+    if (!user) {
+      return res.status(401).json({
+        message: "Unauthorized: Invalid username or password",
+      });
+    }
+    let isPasswordMatch = await comparePassword(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(401).json({
+        message: "Unauthorized: Invalid username or password",
+      });
+    }
+    req.user = user;
+    next();
+  };
+
+  return authorizeToken;
 };
-
-module.exports = authorizeToken;
