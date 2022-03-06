1c4e7cb4364e45d9e28906587cfe143354639629
Soumya Nayar <nayar.s@northeastern.edu>
2022-03-06 13:01:44 -0800

    Update UserData

--- ec2-user-data.sh
+++ ec2-user-data.sh
@@ -8,24 +8,11 @@
     echo "DB_NAME=my_webserver_db"
     echo "PORT=3000"
     echo "AWS_BUCKET_NAME=webservice-bucket-5e1889cd-fabf-4425-b16b-c029145b795e"
-} >>~/webservice/app.env
+} >>/home/ec2-user/webservice/app.env
 
-mkdir ~/.aws
-
-{
-    echo "[default]"
-    echo "region=us-west-2"
-} >>~/.aws/config
-
-{
-    echo "[default]"
-    echo "aws_access_key_id=AKIAT2HPCMSJSSQKND5B"
-    echo "aws_secret_access_key=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"
-} >>~/.aws/credentials
-
-cd ~/webservice || exit
+cd /home/ec2-user/webservice || exit
 npm install
-sudo cp ~/webservice/nodeserver.service /etc/systemd/system/nodeserver.service
+sudo cp /home/ec2-user/webservice/nodeserver.service /etc/systemd/system/nodeserver.service
 
 sudo systemctl enable nodeserver.service
 sudo systemctl start nodeserver.service
