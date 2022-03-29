a39ae7b8c8949071b8eef1e6c70160cc9cd488f3
Soumya Nayar <nayar.s@northeastern.edu>
2022-03-28 19:32:49 -0700

    AfterInstall changes

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -3,10 +3,10 @@
 cd /home/ec2-user
 pwd
 ls -a
-sudo chown -R ec2-user:ec2-user /home/ec2-user/webservice
 sudo unzip webservice.zip
+sudo chown -R ec2-user:ec2-user /home/ec2-user/webservice
 pwd
-sudo cp /home/ec2-user/app.env webservice/app.env
+sudo cp /home/ec2-user/app.env /home/ec2-user/webservice/app.env
 cd /home/ec2-user/webservice
 sudo npm install
 ls -a
