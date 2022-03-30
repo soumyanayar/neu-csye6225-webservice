5a2cc79236e35efc1f43a4d8168f7bfdc2e4d39a
Soumya Nayar <nayar.s@northeastern.edu>
2022-03-29 23:51:34 -0700

    Moving applicationstop commands to beforeinstall

--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+echo "If nodeserver is running, stop it"
+sudo systemctl status nodeserver.service
+if [ $? -eq 0 ]; then
+    echo "Stopping node server service"
+    sudo systemctl stop nodeserver.service
+    echo "Disabling node server service"
+    sudo systemctl disable nodeserver.service
+fi
+
+echo "if any app is running at port 3000, stop it"
+sudo lsof -t -i:3000
+if [ $? -eq 0 ]; then
+    echo "Killing the process at port 3000"
+    sudo kill -9 $(sudo lsof -t -i:3000)
+fi
+
 cd /home/ec2-user
 sudo rm -rf webservice
