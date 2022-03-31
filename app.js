0423f131bfb1a7951281be60a2189d0f4dceea0d
Soumya Nayar <nayar.s@northeastern.edu>
2022-03-31 12:19:24 -0700

    New test path

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ module.exports = (database, s3) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
 
-  app.use("/health1", basicRoutes);
+  app.use("/health2", basicRoutes);
   app.use("/v1/user", userRoutes);
   app.use("/v1/user/self/pic", imageRoutes);
   return app;
