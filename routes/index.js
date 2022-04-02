40bb44805e480823be383d110c992e59d52ef34e
Soumya Nayar <nayar.s@northeastern.edu>
2022-04-01 22:58:59 -0700

    Simple Metric

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const logger = require("../configs/winston");
+var SDC = require("statsd-client");
+const sdc = new SDC({ host: "localhost", port: 8125 });
 
 router.get("", async (req, res) => {
   try {
+    sdc.increment("healthz.get");
     logger.info("GET /healthz - OK");
-    logger.silly(`req.sourceIp: ${req.sourceIp}`);
     res.status(200).send();
   } catch (e) {
     res.status(500).send();
