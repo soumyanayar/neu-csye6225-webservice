274a660387f416f404f38f8b38d70314ef3b51b0
Soumya Nayar <nayar.s@northeastern.edu>
2022-04-01 22:17:26 -0700

    Fix Application Start

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -7,6 +7,9 @@ sudo cp /home/ec2-user/webservice/cloudwatch-config.json /opt/cloudwatch-config.
 # Configure CloudWatch Agent
 echo "Configuring CloudWatch Agent"
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/cloudwatch-config.json -s
+
+sudo mkdir -p /home/ec2-user/webservice/logs
+sudo touch /home/ec2-user/webservice/logs/csye6225.log
 sudo chmod 775 /home/ec2-user/webservice/logs/csye6225.log
 
 echo "Copying the node server service file to /etc/systemd/system/node-server.service"
