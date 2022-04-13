f64d5a0d393699f3bfade89cd8ef1ca71114991b
Soumya Nayar <nayar.s@northeastern.edu>
2022-04-12 23:18:50 -0700

    Add logs to codedeploy

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+echo "After Install Step started"
+echo "changing the directory to /home/ec2-user"
 cd /home/ec2-user
+echo "Printing the current directory"
 pwd
+echo "Printing all the files and folders in the current directory"
 ls -a
 # sudo unzip webservice.zip
+echo "Changing the owner of the webservice folder to ec2-user"
 sudo chown -R ec2-user:ec2-user /home/ec2-user/webservice
+echo "Printing the current directory"
 pwd
+echo "Copying the /home/ec2-user/app.env to /home/ec2-user/webservice/app.env"
 sudo cp /home/ec2-user/app.env /home/ec2-user/webservice/app.env
+echo "Changing the directory to /home/ec2-user/webservice"
 cd /home/ec2-user/webservice
+echo "Calling the npm install command"
 sudo npm install
+echo "npm install completed"
+echo "Printing all the files and folders in the current directory"
 ls -a
+echo "After Install Step Completed"
--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Application Start Step started"
+
 # Copy cloudwatch agent config file
 echo "Copying the cloudwatch agent config file to /opt/cloudwatch-config.json"
 sudo cp /home/ec2-user/webservice/cloudwatch-config.json /opt/cloudwatch-config.json
@@ -8,12 +10,18 @@ sudo cp /home/ec2-user/webservice/cloudwatch-config.json /opt/cloudwatch-config.
 echo "Configuring CloudWatch Agent"
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/cloudwatch-config.json -s
 
+echo "Creating the logs directory in /home/ec2-user/webservice"
 sudo mkdir -p /home/ec2-user/webservice/logs
+echo "Creating the log file csye6225.log in /home/ec2-user/webservice/logs"
 sudo touch /home/ec2-user/webservice/logs/csye6225.log
+echo "Changing the mode of the log file to 775"
 sudo chmod 775 /home/ec2-user/webservice/logs/csye6225.log
 
 echo "Copying the node server service file to /etc/systemd/system/node-server.service"
 sudo cp /home/ec2-user/webservice/nodeserver.service /etc/systemd/system/nodeserver.service
-echo "Enabling and Starting the node server service"
+echo "Enabling the node server service"
 sudo systemctl enable nodeserver.service
+echo "Starting the node server service"
 sudo systemctl start nodeserver.service
+
+echo "Application Start Step Completed"
