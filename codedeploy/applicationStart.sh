d87c9958a2afef0b5179531b8cd6e45040671fed
Soumya Nayar <nayar.s@northeastern.edu>
2022-04-13 13:52:21 -0700

    Changing the command to copy the .env file

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -3,7 +3,7 @@
 echo "Application Start Step started"
 
 echo "Copy the app.env file to /home/ec2-user/webservice/app.env"
-sudo cp /home/ec2-user/webservice/app.env /home/ec2-user/webservice/app.env
+sudo cp /home/ec2-user/app.env /home/ec2-user/webservice/app.env
 
 echo "Enabling the node server service"
 sudo systemctl enable nodeserver.service
